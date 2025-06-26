995fe460b122588d062c0d0e84ef1785051663b0
Kopika Muralidharan <142069450+Kopika0208@users.noreply.github.com>
2025-06-26 10:09:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def download_file(filename):
     return send_from_directory('static/generated_docs', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use $PORT from Render
+    app.run(host="0.0.0.0", port=port)
